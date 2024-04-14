704102f2099a6d353015e5cb4167fafd24e041cb
Silvan Stuber <140338185+SilvanStuber@users.noreply.github.com>
2024-04-14 20:23:36 +0200

    import firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { NoteListComponent } from './note-list/note-list.component';
 import { NoteComponent } from './note-list/note/note.component';
 import { FormsModule } from '@angular/forms';
 import { AddNoteDialogComponent } from './add-note-dialog/add-note-dialog.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 
 @NgModule({
@@ -23,6 +25,8 @@ import { AddNoteDialogComponent } from './add-note-dialog/add-note-dialog.compon
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    provideFirebaseApp(() => initializeApp({"projectId":"danotes-983ae","appId":"1:341657773294:web:83729642fc8737230bbfff","storageBucket":"danotes-983ae.appspot.com","apiKey":"AIzaSyB2OYHN0soX35u81mkTJQWQTU8I4S6X4IA","authDomain":"danotes-983ae.firebaseapp.com","messagingSenderId":"341657773294"})),
+    provideFirestore(() => getFirestore()),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/firebase-services/note-list.service.ts
+++ src/app/firebase-services/note-list.service.ts
@@ -1,10 +1,40 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { Note } from '../interfaces/note.interface'
+import { Firestore, collection, doc, collectionData } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NoteListService {
 
-  constructor() { }
+  trashNotes: Note[] = [];
+  normalNotes: Note[] = [];
+
+  items$;
+  items;
+
+  firestore: Firestore = inject(Firestore);
+
+  constructor() { 
+    this.items$ = collectionData(this.getNotesRef());//import der Daten
+    this.items = this.items$.subscribe((list) => { //subscriben der Daten
+      list.forEach(element => {
+        console.log(element) //mit der forEach greifen wir auf die einzelnen Elemenete zu.
+      });
+    } )
+    this.items.unsubscribe();//am Schluss beenden wir die subscribe gute Sache für unser Memory
+  }
+
+  getNotesRef() {
+    return collection(this.firestore, 'notes');
+  }
+
+  getTrashRef() {
+    return collection(this.firestore, 'trash');
+  }
+
+  getSingleDocRef(colId:string, docId:string) {
+    return doc(collection(this.firestore, colId), docId);
+  }
 }
