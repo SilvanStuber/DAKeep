69e609fa48a13fb949043b8a73262abdbe29da7b
Silvan Stuber <140338185+SilvanStuber@users.noreply.github.com>
2024-04-15 23:32:12 +0200

    deleteDoc()

--- src/app/firebase-services/note-list.service.ts
+++ src/app/firebase-services/note-list.service.ts
@@ -7,6 +7,7 @@ import {
   addDoc,
   onSnapshot,
   updateDoc,
+  deleteDoc,
 } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 
@@ -25,11 +26,16 @@ export class NoteListService {
   constructor() {
     this.unsubTrash = this.subTrashList();
     this.unsubNotes = this.subNotesList();
-
   }
 
-  async addNote(item: {}) {
-    await addDoc(this.getNotesRef(), item) //hier fügen wir der Datenbank den Inhalt welcher beim Aufruf der Funktion übergeben hinzu
+  async addNote(item: Note, colId: "notes" | "trash") {
+    let refFunction: any;
+    if (colId == "notes") {
+      refFunction = this.getNotesRef(); //wenn die collection id notes ist in notes speichern
+    } else if (colId == "trash") {  
+      refFunction = this.getTrashRef();  //wenn die collection id trash ist in trash speichern
+    }
+    await addDoc(refFunction, item) //hier fügen wir der Datenbank den Inhalt welcher beim Aufruf der Funktion übergeben hinzu
       .catch((err) => {
         //hier legen wir fest wenn etwas nicht funktioniert wie er vorgehen soll
         console.error(err);
@@ -43,29 +49,37 @@ export class NoteListService {
   async updateNote(note: Note) {
     if (note.id) {
       let docRef = this.getSingleDocRef(this.getColIdFromNote(note), note.id);
-      await updateDoc(docRef, this.getCleanJson(note)).catch(
-        (err) => {
-          console.log(err);
-        }
-      ); // hier an der stelle macht das then kein sinn
+      await updateDoc(docRef, this.getCleanJson(note)).catch((err) => {
+        console.log(err);
+      }); // hier an der stelle macht das then kein sinn
     }
   }
 
-  getCleanJson(note: Note):{} { //mit :{} definieren wir das wir ein JSON returnen
+  getCleanJson(note: Note): {} {
+    //mit :{} definieren wir das wir ein JSON returnen
     return {
       type: note.type,
       title: note.title,
       content: note.content,
       marked: note.marked,
-    }
+    };
   }
 
   getColIdFromNote(note: Note) {
     if (note.type == 'note') {
       return 'notes';
-    } else {    //hier übersetzen wir das wir den richtigen collection Id haben
+    } else {
+      //hier übersetzen wir das wir den richtigen collection Id haben
       return 'trash';
     }
+  }
+
+  async deleteNote(colId: string, docId: string) {
+    await deleteDoc(this.getSingleDocRef(colId, docId)).catch((err) => {
+      console.log(err);//console.log nur für entwicklung später funktion einfügen evtl anschliessend ein then einfügen
+    }); //hier muss mann nur deklarieren wass gelösch werden muss
+    //und übergeben die collection id und document id welche sich
+    //in der collection befindet
   }
 
   ngonDestroy() {
--- src/app/note-list/note/note.component.ts
+++ src/app/note-list/note/note.component.ts
@@ -1,53 +1,66 @@
 import { Component, Input } from '@angular/core';
 import { Note } from '../../interfaces/note.interface';
-import { NoteListService } from '../../firebase-services/note-list.service'
+import { NoteListService } from '../../firebase-services/note-list.service';
 
 @Component({
   selector: 'app-note',
   templateUrl: './note.component.html',
-  styleUrls: ['./note.component.scss']
+  styleUrls: ['./note.component.scss'],
 })
 export class NoteComponent {
-  @Input() note!:Note;
+  @Input() note!: Note;
   edit = false;
   hovered = false;
-  
-  constructor(private noteService: NoteListService){}
 
-  changeMarkedStatus(){
+  constructor(private noteService: NoteListService) {}
+
+  changeMarkedStatus() {
     this.note.marked = !this.note.marked;
     this.saveNote();
   }
 
-  deleteHovered(){
-    if(!this.edit){
+  deleteHovered() {
+    if (!this.edit) {
       this.hovered = false;
     }
   }
 
-  openEdit(){
+  openEdit() {
     this.edit = true;
   }
 
-  closeEdit(){
+  closeEdit() {
     this.edit = false;
     this.saveNote();
   }
 
-  moveToTrash(){
-    this.note.type = 'trash';
+  moveToTrash() {
+    if (this.note.id) {
+      this.note.type = 'trash'; //note typ ändern wir in trash
+      let docId = this.note.id; //hier übergeben wir die documenten id
+      delete this.note.id; // hiermit löschen wir nicht nur den Inhalt auch die document Id
+      this.noteService.addNote(this.note, 'trash'); //speichern den Inhalt unter der collection trash
+      this.noteService.deleteNote('notes', docId); //löschen die inhalte in der collection notes
+    }
   }
 
-  moveToNotes(){
-    this.note.type = 'note';
+  moveToNotes() {
+    if (this.note.id) {
+      this.note.type = 'note';; //note typ ändern wir in note
+      let docId = this.note.id; //hier übergeben wir die documenten id
+      delete this.note.id; // hiermit löschen wir nicht nur den Inhalt auch die document Id
+      this.noteService.addNote(this.note, 'notes'); //speichern den Inhalt unter der collection notes
+      this.noteService.deleteNote('trash', docId); //löschen die inhalte in der collection trash
+    }
   }
 
-  deleteNote(){
-
+  deleteNote() {
+    if (this.note.id) {
+      this.noteService.deleteNote('trash', this.note.id); //löschen die inhalte in der collection trash
+    }
   }
 
-  saveNote(){
+  saveNote() {
     this.noteService.updateNote(this.note);
   }
-
 }
